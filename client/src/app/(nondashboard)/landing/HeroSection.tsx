f76f7714865179fb1ffb7580f178c7d86ab1ca1a
Alex Assali <alex.assali@gmail.com>
2025-06-09 15:44:02 +0300

    Fixing Search Properties from HeroSection

--- client/src/app/(nondashboard)/landing/HeroSection.tsx
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -11,44 +11,44 @@ import { setFilters } from "@/state";
 
 const HeroSection = () => {
   const dispatch = useDispatch();
-  // const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
   const router = useRouter();
   const pathname = usePathname();
 
-  // const handleLocationSearch = async () => {
-  //   try {
-  //     const trimmedQuery = searchQuery.trim();
-  //     if (!trimmedQuery) return;
+  const handleLocationSearch = async () => {
+    try {
+      const trimmedQuery = searchQuery.trim();
+      if (!trimmedQuery) return;
 
-  //     const response = await fetch(
-  //       `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
-  //         trimmedQuery
-  //       )}.json?access_token=${
-  //         process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
-  //       }&fuzzyMatch=true`
-  //     );
-  //     const data = await response.json();
-  //     if (data.features && data.features.length > 0) {
-  //       console.log("features", data.features[0]);
-  //       const [lng, lat] = data.features[0].center;
-  //       dispatch(
-  //         setFilters({
-  //           location: trimmedQuery,
-  //           coordinates: [lat, lng],
-  //         })
-  //       );
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+          trimmedQuery
+        )}.json?access_token=${
+          process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+        }&fuzzyMatch=true`
+      );
+      const data = await response.json();
+      if (data.features && data.features.length > 0) {
+        console.log("features", data.features[0]);
+        const [lng, lat] = data.features[0].center;
+        dispatch(
+          setFilters({
+            location: trimmedQuery,
+            coordinates: [lng, lat],
+          })
+        );
 
-  //       const params = new URLSearchParams({
-  //         location: trimmedQuery,
-  //         lat: lat.toString(),
-  //         lng: lng,
-  //       });
-  //       router.push(`/search?${params.toString()}`);
-  //     }
-  //   } catch (error) {
-  //     console.error("error search location:", error);
-  //   }
-  // };
+        const params = new URLSearchParams({
+          location: trimmedQuery,
+          lat: lat.toString(),
+          lng: lng,
+        });
+        router.push(`/search?${params.toString()}`);
+      }
+    } catch (error) {
+      console.error("error search location:", error);
+    }
+  };
 
   return (
     <div className="relative h-screen">
@@ -76,16 +76,16 @@ const HeroSection = () => {
           </p>
 
           <div className="flex justify-center">
-            {/* <Input
+            <Input
               type="text"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
               placeholder="Search by city, neighborhood or address"
               className="w-full max-w-lg rounded-none rounded-l-xl border-none bg-white h-12"
-            /> */}
+            />
             <Button
-              onClick={() => router.push(`/search`)}
-              className="w-full max-w-lg bg-secondary-500 text-white rounded-xl border-none hover:bg-secondary-600 h-12"
+              onClick={handleLocationSearch}
+              className="bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12"
             >
               Search
             </Button>
